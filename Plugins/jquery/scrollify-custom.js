26a4d2f74ec868fe5b5a7bf4b733953b9c6a6887
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-06-17 16:12:24 -0300

    nova atividade

---
+++ Plugins/jquery/scrollify-custom.js
@@ -0,0 +1,16 @@
+$(function() {
+	
+    $.scrollify({
+        section : ".panel",
+      interstitialSection : ".footer",
+        easing: "easeOutExpo",
+        scrollSpeed: 800,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        before:function() {},
+        after:function() {},
+        afterResize:function() {},
+        afterRender:function() {}
+      });
+  });
